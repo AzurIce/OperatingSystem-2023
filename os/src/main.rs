fa8e4047ab05093f8b66fea6aca88ae7f4efc31f
Azur冰弦 <973562770@qq.com>
2023-10-20 11:25:42 +0800

    exp2: implemented exit mechanism

--- os/src/main.rs
+++ os/src/main.rs
@@ -2,6 +2,27 @@
 #![no_main]
 
 use core::panic::PanicInfo;
+use core::arch::asm;
+
+const SYSCALL_EXIT: usize = 93;
+
+fn syscall(id: usize, args: [usize; 3]) -> isize {
+    let mut ret: isize;
+    unsafe {
+        asm!("ecall",
+             in("x10") args[0],
+             in("x11") args[1],
+             in("x12") args[2],
+             in("x17") id,
+             lateout("x10") ret
+        );
+    }
+    ret
+}
+
+pub fn sys_exit(xstate: i32) -> isize {
+    syscall(SYSCALL_EXIT, [xstate as usize, 0, 0])
+}
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
@@ -10,5 +31,5 @@ fn panic(_info: &PanicInfo) -> ! {
 
 #[no_mangle]
 extern "C" fn _start() {
-    loop{};
+    sys_exit(9);
 }
