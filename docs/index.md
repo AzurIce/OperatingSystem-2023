e230acdc0f36135bc16479fa79ad66339dbe2cb2
Azur冰弦 <973562770@qq.com>
2023-11-18 13:21:49 +0800

    exp3: by 21301021 肖斌

--- docs/index.md
+++ docs/index.md
@@ -1,3 +1,7 @@
 BJTU OperatingSystem-2023
 
 相关实验记录文档都会发布在这里
+
+
+
+待啃：https://mit-public-courses-cn-translatio.gitbook.io/mit6-s081/
--- os/src/main.rs
+++ os/src/main.rs
@@ -1,15 +1,20 @@
 #![no_std]
 #![no_main]
 #![feature(panic_info_message)]
-#[macro_use]
 
+#[macro_use]
 mod console;
 mod lang_items;
 mod sbi;
+mod batch;
+mod trap;
+mod syscall;
 
 use core::arch::global_asm;
 
 global_asm!(include_str!("entry.asm"));
+global_asm!(include_str!("link_app.S"));
+
 fn clear_bss() {
     extern "C" {
         fn sbss();
@@ -20,28 +25,9 @@ fn clear_bss() {
 
 #[no_mangle]
 pub fn rust_main() -> ! {
-    extern "C" {
-        fn stext();
-        fn etext();
-        fn srodata();
-        fn erodata();
-        fn sdata();
-        fn edata();
-        fn sbss();
-        fn ebss();
-        fn boot_stack();
-        fn boot_stack_top();
-    }
     clear_bss();
-    println!("Hello, world!");
-    println!(".text [{:#x}, {:#x})", stext as usize, etext as usize);
-    println!(".rodata [{:#x}, {:#x})", srodata as usize, erodata as usize);
-    println!(".data [{:#x}, {:#x})", sdata as usize, edata as usize);
-    println!(
-        "boot_stack [{:#x}, {:#x})",
-        boot_stack as usize, boot_stack_top as usize
-    );
-    println!(".bss [{:#x}, {:#x})", sbss as usize, ebss as usize);
-    println!("Hello, world!");
-    panic!("Shutdown machine!");
+    println!("[Kernel] Hello, world!");
+    trap::init();
+    batch::init();
+    batch::run_next_app();
 }
